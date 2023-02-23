4315730d555205ed383a55dd6d2be30960ca0f90
Lucheng Zhang <geekLucian@gmail.com>
2023-02-23 22:54:35 +0800

    Train ready

--- matd3/environments/myenv/make_env.py
+++ matd3/environments/myenv/make_env.py
@@ -1,7 +1,6 @@
-# from matd3.environments.myenv.env_new import MyEnv
-from matd3.environments.myenv.env_range import RangeEnv
+from matd3.environments.myenv.env_new import MyEnv
 
 
 def make_env():
     # todo：补全，传入必要参数去初始化训练场景
-    return RangeEnv()
+    return MyEnv()
