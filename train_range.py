49486c15f7eef093f762b462796d47d78737679a
Lucheng Zhang <geekLucian@gmail.com>
2023-03-06 23:33:55 +0800

    improve printing style

--- train_range.py
+++ train_range.py
@@ -3,18 +3,21 @@ import time
 
 import numpy as np
 import tensorflow as tf
+from datetime import datetime
 
 from matd3.environments.myenv.env_range import RangeEnv
 from matd3.agents.matd3 import MATD3Agent
 from matd3.common.logger import RLLogger
+from matd3.common.util import info
 
 # 设置GPU显存动态分配
-
 if tf.config.experimental.list_physical_devices('GPU'):
     tf.config.experimental.set_memory_growth(tf.config.experimental.list_physical_devices('GPU')[0], True)
 
+now = datetime.now().strftime("-%Y-%m-%d-%H-%M-%S")
 
-def train(exp_name='multi_agent',  # 指定本次实验的命名
+
+def train(exp_name='range_pricing' + now,  # 指定本次实验的命名
           save_episode_rate=20,  # 指定多久保存一次模型，输出结果
           display=False,  # 是否渲染环境
           restore_filepath=None,  # 指定加载已存在模型的路径，比如 "results/xxx/models"
@@ -57,20 +60,22 @@ def train(exp_name='multi_agent',  # 指定本次实验的命名
             agent.load(fp)
 
     # 重置环境
-    obs_n, combine_reward0, clear_price = env.reset()
+    obs_n, combine_reward0, clear_price, end_reason = env.reset()
 
     # 开始迭代收集数据，训练
     print('Starting iterations...')
     while True:
         # get action
         if logger.episode_count > 180:
-            action_n = [agent.action(obs.astype(np.float32), 0.2 / (logger.train_step / 1500 + 1)) for agent, obs in zip(agents, obs_n)]
+            action_n = [agent.action(obs.astype(np.float32), 0.2 / (logger.train_step / 1500 + 1)) for agent, obs in
+                        zip(agents, obs_n)]
         else:
             action_n = [agent.policy.sample_action().numpy() for agent, obs in zip(agents, obs_n)]
             # print(action_n)
 
         # 执行动作
-        new_obs_n, profit, clear_price, total_match_volume, seller_clear_volume, buyer_clear_volume, match_result, seller_profit, done, _ = env.step(action_n)
+        (new_obs_n, profit, clear_price, total_match_volume, seller_clear_volume, buyer_clear_volume, match_result,
+         seller_profit, end_reason, done, _) = env.step(action_n)
         # 生成新状态、总利润、统一出清价格、总成交电量、售电方成交电量、购电方成交量、成交结果矩阵、标志位
 
         rew = 0 * profit + 1 * total_match_volume
@@ -93,20 +98,22 @@ def train(exp_name='multi_agent',  # 指定本次实验的命名
         # 判断episode是否结束
         if done:
             if logger.episode_count % 100 == 0:
-                print('最终出清价格-每一幕', clear_price)
-                print("最终匹配对-每一幕", match_result)
+                info("平均价格:\n [seller0_avg_price, seller1_avg_price, ..., buyer0_avg_price]\n" + str(clear_price))
+                info("交易记录:\n [[buyer_name, seller_name, match_volume, match_price]]\n\t\b" +
+                     '\n\t'.join(map(str, match_result)))
+                info("共{}条，出清中止原因：{}".format(len(match_result), end_reason))
             for i in range(env.num_of_seller):
                 logger.log_scalar('seller_{}/profit'.format(i), seller_profit[i], step_type='episode')
                 logger.log_scalar('seller_{}/volume'.format(i), seller_clear_volume[i], step_type='episode')
                 logger.log_scalar('seller_{}/price'.format(i), clear_price[i], step_type='episode')
             for i in range(env.num_of_buyer):
                 logger.log_scalar('buyer_{}/volume'.format(i), buyer_clear_volume[i], step_type='episode')
-                logger.log_scalar('buyer_{}/price'.format(i), clear_price[i+env.num_of_seller], step_type='episode')
+                logger.log_scalar('buyer_{}/price'.format(i), clear_price[i + env.num_of_seller], step_type='episode')
             # logger.log_scalar('training/clear_price', clear_price, step_type='episode')
             logger.log_scalar('training/total_profit', profit, step_type='episode')
             logger.log_scalar('training/total_match_volume', total_match_volume, step_type='episode')
 
-            obs_n, combine_reward0, clear_price = env.reset()  # 重置场景，拿到新的状态
+            obs_n, combine_reward0, clear_price, end_reason = env.reset()  # 重置场景，拿到新的状态
             logger.record_episode_end(agents)  # 记录数据，重置某些变量，输出到tensorboard
 
         # 更新策略，经验充足
