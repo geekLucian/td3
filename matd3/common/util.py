69fdc2336ddaec664b7453d9353cea9e89ebead0
Lucheng Zhang <geekLucian@gmail.com>
2023-03-06 23:33:55 +0800

    improve printing style

--- matd3/common/util.py
+++ matd3/common/util.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 
 
+def info(msg):
+    """Print green information using ANSI code"""
+    print("\033[92m {}\033[00m".format(msg))
+
+
 def clip_by_local_norm(gradients, norm):
     """
     Clips gradients by their own norm, NOT by the global norm
